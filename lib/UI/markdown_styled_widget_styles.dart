e4dd6bf76d0e5e45fcc262b84a8aa9aef2691c62
Willsem <happymainer@gmail.com>
2022-07-23 18:06:52 +0300

    update styles for widget

--- lib/UI/markdown_styled_widget_styles.dart
+++ lib/UI/markdown_styled_widget_styles.dart
@@ -1,44 +1,102 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class MarkdownStyledWidgetStyles {
-  // Default text
-  final TextStyle paragraph; // text
+  final int fontSize;
 
-  // Bold text
-  final TextStyle strong; // __text__ or **text**
+  final String defaultFont;
+  final int defaultWeight;
+  final Color defaultForeground;
 
-  // Italic text
-  final TextStyle italic; // _text_ or *text*
+  final int boldWeight;
+  final Color boldForeground;
 
-  // Headers
-  final TextStyle heading1; // # text
-  final TextStyle heading2; // ## text
-  final TextStyle heading3; // ### text
-  final TextStyle heading4; // #### text
-  final TextStyle heading5; // ##### text
-  final TextStyle heading6; // ###### text
+  final Color linkForeground;
 
-  // Blockquote
-  final TextStyle blockquote; // > text
+  final String blockquoteFont;
+  final int blockquoteSize;
+  final Color blockquoteBackground;
+  final Color blockquoteForeground;
+  final Color blockquoteBorderColor;
+  final int blockquoteBorderSize;
 
-  // Links
-  final TextStyle link; // [text](link)
+  final Color codeBackground;
+  final Color codeForeground;
+  final String codeFont;
+  final int codeWeight;
 
-  // Code
-  final TextStyle code; // `text` or ```text```
+  final int heading1Size;
+  final bool heading1Line;
+  final int heading1Weight;
+
+  final int heading2Size;
+  final bool heading2Line;
+  final int heading2Weight;
+
+  final int heading3Size;
+  final bool heading3Line;
+  final int heading3Weight;
+
+  final int heading4Size;
+  final bool heading4Line;
+  final int heading4Weight;
+
+  final int heading5Size;
+  final bool heading5Line;
+  final int heading5Weight;
+
+  final int heading6Size;
+  final bool heading6Line;
+  final int heading6Weight;
+
+  final int lineHeight;
 
   const MarkdownStyledWidgetStyles({
-    this.paragraph = const TextStyle(),
-    this.strong = const TextStyle(),
-    this.italic = const TextStyle(),
-    this.heading1 = const TextStyle(),
-    this.heading2 = const TextStyle(),
-    this.heading3 = const TextStyle(),
-    this.heading4 = const TextStyle(),
-    this.heading5 = const TextStyle(),
-    this.heading6 = const TextStyle(),
-    this.blockquote = const TextStyle(),
-    this.link = const TextStyle(),
-    this.code = const TextStyle(),
+    this.fontSize = 8,
+    this.defaultFont = 'Roboto',
+    this.defaultWeight = 400,
+    this.defaultForeground = Colors.black,
+
+    this.boldWeight = 700,
+    this.boldForeground = Colors.black54,
+
+    this.linkForeground = Colors.blueAccent,
+
+    this.blockquoteFont = 'IbarraRealNova',
+    this.blockquoteSize = 8,
+    this.blockquoteBackground = Colors.grey,
+    this.blockquoteForeground = Colors.black54,
+    this.blockquoteBorderColor = Colors.blueGrey,
+    this.blockquoteBorderSize = 10,
+
+    this.codeBackground = Colors.grey,
+    this.codeForeground = Colors.black26,
+    this.codeFont = 'RobotoMono',
+    this.codeWeight = 400,
+
+    this.heading1Size = 24,
+    this.heading1Line = true,
+    this.heading1Weight = 500,
+
+    this.heading2Size = 20,
+    this.heading2Line = true,
+    this.heading2Weight = 500,
+
+    this.heading3Size = 16,
+    this.heading3Line = false,
+    this.heading3Weight = 500,
+
+    this.heading4Size = 12,
+    this.heading4Line = false,
+    this.heading4Weight = 500,
+
+    this.heading5Size = 8,
+    this.heading5Line = false,
+    this.heading5Weight = 500,
+
+    this.heading6Size = 6,
+    this.heading6Line = false,
+    this.heading6Weight = 500,
+
+    this.lineHeight = 1,
   });
 }
